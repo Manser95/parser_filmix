7f209bf84efc7f749d5c5760c4d8c2d2ac232f61
Manser95 <manserb95@gmail.com>
2021-01-03 16:43:07 +0300

    base parse

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import requests
 
-URL = 'https://filmix.co'
+URL = 'https://filmix.co/filmy/'
+FILE_FILMS = 'filmi.csv'
 
-req = requests.get(URL)
+Headers = {
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+    'accept': '*/*'
+}
 
-print(req.status_code)
+def get_html(url, params=None):
+    r = requests.get(url, headers=Headers, params=params)
+    return r
+
+def parser():
+    html = get_html(URL)
+    print(html.status_code)
+
+parser()
